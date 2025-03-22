4b157c5e5fcffbbfe5aff7a3a3a0fb41bf6bf72e
umikok7 <umikok7@gmail.com>
2025-03-22 13:43:23 +0000

    添加了自动编译的脚本

---
+++ build/src/server/CMakeFiles/ChatServer.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "/home/umiko/Project/Muduo_CPP/chatServer/bin/ChatServer"
+  "/home/umiko/Project/Muduo_CPP/chatServer/bin/ChatServer.pdb"
+  "CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "CMakeFiles/ChatServer.dir/chatserver.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "CMakeFiles/ChatServer.dir/chatservice.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "CMakeFiles/ChatServer.dir/db/db.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/main.cpp.o"
+  "CMakeFiles/ChatServer.dir/main.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/model/frrendModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/frrendModel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/model/offlineMessageModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/offlineMessageModel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/model/userModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/userModel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  "CMakeFiles/ChatServer.dir/redis/redis.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
